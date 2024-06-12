c81c4bdb95fe484fcd10cd581e7ad8463ee72c91
Claire Wanjiru <clairewanjiru.m@gmail.com>
2024-06-12 21:38:41 +0300

    made color randomizer

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  function getRandomColor() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    console.log(bgColor);
+    return bgColor;
+  }
+
+  getRandomColor();
+
+  // Assuming that changeColorBtn and colorBox are defined in the HTML
+  let changeColorBtn = document.getElementById("change-color-btn");
+  let colorBox = document.getElementById("color-box");
+
+  changeColorBtn.addEventListener("click", () => {
+    colorBox.style.backgroundColor = getRandomColor();
+  });
+});
